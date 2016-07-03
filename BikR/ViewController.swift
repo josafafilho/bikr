54d5fd57a835b1467d813f25d4301eef2364a43b
Josafá Filho <josafafilho15@gmail.com>
2016-07-03 09:32:51 -0300

    Initial Commit

---
+++ BikR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikR
+//
+//  Created by Josafá Filho on 7/3/16.
+//  Copyright © 2016 Josafá Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
